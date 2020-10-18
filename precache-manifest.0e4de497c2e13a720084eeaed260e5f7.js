5102550d1ce50667eb5fa4d3e5b47ce5c9e49b19
woyaojinzuzhi <2801097664@qq.com>
2020-10-18 10:23:02 +0800

    zuoye2

---
+++ precache-manifest.0e4de497c2e13a720084eeaed260e5f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d80ca0eb7851e3e0151692b6793ab5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0398d8dc0cfea2167ead",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "1bc162dbe3cedf1d8e9f",
+    "url": "/static/css/main.32529c98.chunk.css"
+  },
+  {
+    "revision": "0398d8dc0cfea2167ead",
+    "url": "/static/js/2.8da9dae0.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.8da9dae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc162dbe3cedf1d8e9f",
+    "url": "/static/js/main.af601507.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
